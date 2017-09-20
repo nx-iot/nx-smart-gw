a5a1648797895cf918a9bc46a6a88237dd6f3791
Somchit Chanudom <somchit.c@Somchits-Mac-mini.local>
2017-09-21 00:59:49 +0700

    test xxx

--- test_xbee.py
+++ test_xbee.py
@@ -5,23 +5,18 @@ import serial
 import time
 
 ser = serial.Serial('/dev/ttyS1', 38400)
-AI_COMMAND = ''.join(chr(x) for x in [0x7E, 0x00, 0x04, 0x08, 0x01, 0x41, 0x49, 0x6C])
-SL_COMMAND  = ''.join(chr(x) for x in [0x7E, 0x00, 0x04, 0x08, 0x01, 0x53, 0x4C, 0x57])
-SH_COMMAND   = ''.join(chr(x) for x in 0x7E, 0x00, 0x04, 0x08, 0x01, 0x53, 0x48, 0x5B])
+COMMAND[0] = ''.join(chr(x) for x in [0x7E, 0x00, 0x04, 0x08, 0x01, 0x41, 0x49, 0x6C])
+COMMAND[1]  = ''.join(chr(x) for x in [0x7E, 0x00, 0x04, 0x08, 0x01, 0x53, 0x4C, 0x57])
+COMMAND[2]   = ''.join(chr(x) for x in [0x7E, 0x00, 0x04, 0x08, 0x01, 0x53, 0x48, 0x5B])
 
-ser.send(AI_COMMAND)
-time.sleep(1)
-while ser.inWaiting() > 0:
-    print ser.read(1)
+str_cmd=0
 
 
-ser.send(SL_COMMAND)
-time.sleep(1)
-while ser.inWaiting() > 0:
-    print ser.read(1)
+while True:
+    if str_cmd < 3:
+        ser.write(COMMAND[str_cmd])
+        str_cmd=str_cmd+1
 
-
-ser.send(SH_COMMAND)
-time.sleep(1)
-while ser.inWaiting() > 0:
-    print ser.read(1)
+    bytesToRead = ser.inWaiting()
+    reps=ser.read(bytesToRead)
+    print reps
